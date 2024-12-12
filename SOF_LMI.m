073bad176c65cfc84e0bacc6bbe841b17fd29b90
Corrado-possieri <possieri@ing.uniroma2.it>
2024-12-12 17:13:39 +0100

    Update SOF_LMI.m

--- SOF_LMI.m
+++ SOF_LMI.m
@@ -4,30 +4,41 @@ clc
 
 % name of the example to be tested
 exName =  'AC4';
+epsilon = 1e-3;
 % – Load COMPleib test example 
 [A,B1,B,C1,C,D11,D12,D21,nx,nw,nu,nz,ny] = COMPleib(exName);
 % define Cz and Dz
 Cz = C;
 Dz = eye(ny,nu);
 % check if the methid can be applied
-if norm(Dz'*Dz - eye(nu)) < 1e-3
-    disp(exName)
-    % define the LMIs
-    P = sdpvar(nx,nx);
-    K = sdpvar(nu,ny);
-    M = [P*A+A'*P+Cz'*Dz*K*C+C'*K'*Dz'*Cz, P*B+C'*K';
-        B'*P+K*C, -eye(nu)];
-    LMI = [P >= 0; M <= 0];
-    options = sdpsettings('solver','mosek','verbose',0);
-    % solve the LMIs
-    sol = optimize(LMI,[],options);
-    % check if the determined solution is stabilizing
-    KK = value(K);
-    if max(real(eig(A+B*KK*C))) < 0
-        LMItiming = sol.solvertime;
-    else
-        LMItiming = -2;
-    end
+W = sdpvar(nx,nx);
+M = sdpvar(ny,ny);
+N = sdpvar(nu,ny);
+LMI = [A*W + W*A' - B*N*C - C'*N'*B' <= -epsilon*eye(nx),...
+    W >= epsilon*eye(nx), M*C==C*W];
+options = sdpsettings('solver','mosek','verbose',0);
+% solve the LMIs
+sol = optimize(LMI,[],options);
+% check if the determined solution is stabilizing
+NN = value(N);
+MM = value(M);
+KK = - NN/MM;
+if max(real(eig(A+B*KK*C))) < 0
+    LMItiming = sol.solvertime;
 else
-    LMItiming = -1;
+    partialTime = sol.solvertime;
+    P = sdpvar(nx,nx);
+    M = sdpvar(nu,nu);
+    N = sdpvar(nu,ny);
+    LMI = [P*A + A'*P - B*N*C - C'*N'*B' <= -epsilon*eye(nx),...
+        P >= epsilon*eye(nx), B*M==W*B];
+    sol = optimize(LMI,[],options);
+    NN = value(N);
+    MM = value(M);
+    KK = - MM\NN;
+    if max(real(eig(A+B*KK*C))) < 0
+        LMItiming(i) = partialTime + sol.solvertime;
+    else
+        LMItiming(i) = -2;
+    end
 end
